0ecbbfc325023ccaebe0b56a66369fceb10d6586
Hyosun Lee <hyosunlee@Hyosuns-MacBook-Pro.local>
2022-08-11 16:52:14 +0100

    add initial state into a function so that it loaded only once when the page loaded and avoid unnecessary re-rendering

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -7,7 +7,7 @@ const nameInLocalStorage = window.localStorage.getItem('name')
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = useState(nameInLocalStorage ?? initialName)
+  const [name, setName] = useState(() => nameInLocalStorage ?? initialName)
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
